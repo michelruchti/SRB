b11bbdbb58a5c633458f74e30c60adfd0ec966f8
michelruchti <46570285+michelruchti@users.noreply.github.com>
2020-01-22 23:31:14 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,87 +1,65 @@
 #!/bin/bash
 
-# IMPORTANT
-# Create a new user with sudo privileges:
-# adduser username
-# usermod -aG sudo username
-# Copy ssh key: ssh-copy-id username@x.x.x.x
-# Enable public key login 
-# Disable root login and password auth in /etc/ssh/sshd_config 
-
 sudo -v
 
 # Variables:
+COLOR="\e[95m" 
 GO_VERSION="go1.13.5.linux-amd64.tar.gz"
+AQUATONE_VERSION="aquatone_linux_amd64_1.7.0.zip"
 
 # Setup script
-printf "\n\n------------------------------------------\n"
-printf "Hello, $USER. Let's configure this system.\n"
-printf "-------- SRB ----- setup.sh v1.0 ---------\n\n"
-sleep 5
+echo -e "${COLOR}\n\n----------------------------------------------\n"
+echo -e "${COLOR}--- Hello, $USER. Let's setup this system. ----\n"
+echo -e "${COLOR}----------------------------------------------\n\n"
+sleep 3
 
 # Setup firewall
-echo "Setup firewall ..."
+echo -e "${COLOR}[*] Setting up ufw firewall"
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow 22
 sudo ufw allow 80
 sudo ufw allow 443
 echo "y" | sudo ufw enable
-echo "Done."
 
 # Update system
-echo "Updating System ..."
-sudo apt update;
+echo -e "${COLOR}[*] Updating system"
+sudo apt update
 sudo apt upgrade -y
 sudo apt dist-upgrade -y
-echo "Done."
-
-# Clean system
-echo "Clean up System ..."
-sudo apt autoremove -y
-sudo apt autoclean -y
-sudo updatedb
-echo "Done."
 
 # Install dependencies
-echo "Installing dependencies ..."
+echo -e "${COLOR}[*] Installing Essentials"
 sudo apt install -y build-essential
 sudo apt install -y python3-pip
 sudo apt install -y python-setuptools
 sudo apt install -y jq
 sudo apt install -y git
-sudo apt install -y nmap
-sudo apt install -y chromium-browser
-sudo apt-get install httpie -y
-sudo apt install curl libcurl4-openssl-dev make zlib1g-dev gawk g++ gcc libreadline6-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 autoconf libgdbm-dev libncurses5-dev automake libtool bison pkg-config ruby ruby-bundler ruby-dev -y
-echo "Done."
+sudo apt install -y httpie
 
-echo "Installing Z-Shell (Oh My Zsh) ..."
-sudo apt install zsh -y
-sudo apt install powerline fonts-powerline -y
+# ZSH & Oh My Zsh
+echo -e "${COLOR}[*] Installing Z-Shell (Oh My Zsh)"
+sudo apt install -y zsh
+sudo apt install -y powerline fonts-powerline
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
 sed -i 's/# ENABLE_CORRECTION/ENABLE_CORRECTION/g' ~/.zshrc
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
 zsh
-chsh -s $(which zsh)
-echo "Done."
+sudo chsh -s $(which zsh)
 
 # Setup Aliases ~/
-echo "Setup Aliases ..."
+echo -e "${COLOR}[*] Setup Aliases"
 echo 'alias webserver="python3 -m http.server 443"' >> ~/.zshrc
-echo "Done."
 
 # Create tools folder in ~/
-echo "Creating tools folder ..."
+echo -e "${COLOR}[*] Creating tools folder"
 mkdir ~/tools
-echo "Done."
 
 # Install Go
-echo "Installing Golang ..."
+echo -e "${COLOR}[*] Installing Golang"
 cd ~/
 wget "https://dl.google.com/go/$GO_VERSION"
 tar -xvf $GO_VERSION
@@ -94,87 +72,82 @@ echo 'export GOPATH=$HOME/go'   >> ~/.zshrc
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$PATH' >> ~/.zshrc
 rm $GO_VERSION
 zsh
-echo "Done."
+
+# Nmap
+echo -e "${COLOR}[*] Installing Nmap"
+sudo apt install -y nmap
+
+# awscli
+echo -e "${COLOR}[*] Installing awscli"
+sudo apt install -y awscli
 
 # Install crtndstry
+echo -e "${COLOR}[*] Installing crtndstry"
 cd ~/tools/
-echo "Installing crtndstry"
 git clone https://github.com/nahamsec/crtndstry.git
-echo "Done."
 
 # Install sublist3r
-echo "Installing sublist3r"
+echo -e "${COLOR}[*] Installing Sublist3r"
 git clone https://github.com/aboul3la/Sublist3r.git
 cd Sublist3r
 sudo pip3 install -r requirements.txt
-cd ~/tools/
-echo "Done."
 
 # Install Amass
-echo "Installing Amass"
+echo -e "${COLOR}[*] Installing Amass"
 cd ~/
 export GO111MODULE=on
 go get -v -u github.com/OWASP/Amass/v3/...
-echo "Done."
 
-echo "installing massdns"
+echo -e "${COLOR}[*] Installing massdns"
 cd ~/tools/
 git clone https://github.com/blechschmidt/massdns.git
 cd ~/tools/massdns
 make
-sudo mv bin/massdns /bin
-echo "done"
+#sudo mv bin/massdns /bin
+ln -sf ~/tools/massdns/bin/massdns /usr/local/bin/massdns
 
 # Install Aquatone
+echo -e "${COLOR}[*] Installing Aquatone"
+sudo apt install -y chromium-browser
 cd ~/
-echo "Installing aquatone ..."
-wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/aquatone_linux_amd64_1.7.0.zip
-unzip aquatone_linux_amd64_1.7.0.zip
-sudo mv aquatone /bin
-rm aquatone_linux_amd64_1.7.0.zip LICENSE.txt README.md
-echo "Done."
+wget https://github.com/michenriksen/aquatone/releases/download/v1.7.0/${AQUATONE_VERSION}
+unzip ${AQUATONE_VERSION}
+ln -sf ~/tools/aqutone /usr/local/bin/aqutone
+rm ${AQUATONE_VERSION} LICENSE.txt README.md
 
 # Install Dirsearch
+echo -e "${COLOR}[*] Installing Dirsearch"
 cd ~/tools/
-echo "Installing dirsearch ..."
 git clone https://github.com/maurosoria/dirsearch.git
-echo "Done."
 
 # Install ffuf
+echo -e "${COLOR}[*] Installing ffuf"
 cd ~/
-echo "Installing ffuf ..."
 go get github.com/ffuf/ffuf
-echo "Done."
 
 # Install Gobuster
+echo -e "${COLOR}[*] Installing Gobuster"
 cd ~/
-echo "Installing Gobuster ..."
 go get github.com/OJ/gobuster
-echo "Done."
 
 # Install sqlmap
-echo "Installing sqlmap ..."
-cd ~/tools
-git clone --depth 1 https://github.com/sqlmapproject/sqlmap.git sqlmap-dev
-echo "Done."
+echo -e "${COLOR}[*] Installing sqlmap"
+sudo apt install -y sqlmap
 
 # Install knock.py
 # echo "Installing knock.py ..."
 # cd ~/tools
 # git clone https://github.com/guelfoweb/knock.git
-# echo "Done."
 
 # Install httprobe
-echo "Installing httprobe ..."
+echo -e "${COLOR}[*] Installing httprobe"
 cd ~/
 go get -u github.com/tomnomnom/httprobe 
-echo "Done."
 
 # Install XSStrike
-echo "Installing XSStrike .."
+echo -e "${COLOR}[*] Installing XSStrike"
 cd ~/tools/
 git clone https://github.com/s0md3v/XSStrike.git
-echo "Done."
 
 # Install JSParser
 # echo "Installing JSParser .."
@@ -182,58 +155,52 @@ echo "Done."
 # git clone https://github.com/nahamsec/JSParser.git
 # cd JSParser
 # python setup.py install
-# echo "Done."
 
 # Install WPScan
-echo "Installing wpscan ..."
+echo -e "${COLOR}[*] Installing WPScan"
+sudo apt install -y libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev libgmp-dev zlib1g-dev 
 cd ~/tools/
-git clone https://github.com/wpscanteam/wpscan.git
-cd wpscan/
-sudo gem install wpscan
-wpscan --update
-echo "Done."
+git clone https://github.com/wpscanteam/wpscan.git 
+cd wpscan/ 
+gem install bundler && bundle install --without test 
+gem install wpscan
 
 # Get Seclists
-echo "Downloading Seclists"
+echo -e "${COLOR}[*] Downloading SecLists"
 cd ~/tools/
 git clone https://github.com/danielmiessler/SecLists.git
 cd ~/tools/SecLists/Discovery/DNS/
 ##THIS FILE BREAKS MASSDNS AND NEEDS TO BE CLEANED
 cat dns-Jhaddix.txt | head -n -14 > clean-jhaddix-dns.txt
-echo "Done."
 
 # Optional: Install DNS Bind Server
 # DNS configs:
 # A	    ns1.domain.com  ->  157.245.24.77       1800 
 # NS	    x.domain.com    ->  ns1.domain.com      1800
-echo "Would you like to install DNS Bind Server for Out-of-Band testing?"
-PS1="Please select an option : "
-options=("yes" "no")
-select opt in "${options[@]}"; do
-        case $opt in
-                yes)
-                        git clone https://github.com/JuxhinDB/OOB-Server.git
-                        cd OOB-Server
-                        echo "Enter your hostname: "
-                        read domain
-                        echo "Enter your server IP: "
-                        read ip
-                        setup $domain $ip
-                        cd ~/tools/
-                        rm -rf OOB-Server/
-                        sudo ufw allow 53
-                        echo "OOB DNS Bind Server installed."
-                        break
-                        ;;
-                no)
-                        echo "Ok."
-                        break
-                        ;;
-                *)      echo "invalid option $REPLY";;
-        esac
-done
+read -p "${COLOR}Would you like to install DNS Bind Server for Out-of-Band testing? y/n"  -n 1 -r
+echo 
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    git clone https://github.com/JuxhinDB/OOB-Server.git
+    cd OOB-Server
+    echo "Enter your hostname: "
+    read domain
+    echo "Enter your server IP: "
+    read ip
+    setup $domain $ip
+    cd ~/tools/
+    rm -rf OOB-Server/
+    sudo ufw allow 53
+    echo "OOB DNS Bind Server installed."
+fi
 
+# Cleaning up system
+echo -e "${COLOR}[*] Cleaning up system"
 cd ~/
 rm -rf SRB/
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo updatedb
 
-printf "\n\n\nDone! All tools are set up in ~/tools\n\n\n"
+echo -e "${COLOR}\n\n\nInstalation complete!\n"
+echo -e "${COLOR}Your tools have been installed in: ${HOME}/tools\n\n\n"
